2b9909906b4677dc2e5eff8c662f54fe0d8c1f5d
Jhon <jftrujillo@unicauca.edu.co>
2016-05-31 11:22:56 -0500

    Add ExtraInfoDao and model Extrainfo: Anotation: Creatin table in azure mobilse service

--- app/src/main/java/com/felkertech/n/cumulustv/model/Extrainfo.java
+++ app/src/main/java/com/felkertech/n/cumulustv/model/Extrainfo.java
@@ -5,4 +5,42 @@ package com.felkertech.n.cumulustv.model;
  */
 public class Extrainfo {
     String title;
+    String description;
+    String imgurl;
+    String msgnot;
+
+//region getters and setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImgurl() {
+        return imgurl;
+    }
+
+    public void setImgurl(String imgurl) {
+        this.imgurl = imgurl;
+    }
+
+    public String getMsgnot() {
+        return msgnot;
+    }
+
+    public void setMsgnot(String msgnot) {
+        this.msgnot = msgnot;
+    }
+
+    //endregion
 }
